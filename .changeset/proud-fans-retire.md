fb57fa3f74d4ebe71ef930f0df75a1bb4e4d16ed
Nick Arocho <nicaroch@amazon.com>
2022-01-26 14:48:34 -0800

    remove unused logger functions from core

---
+++ .changeset/proud-fans-retire.md
@@ -0,0 +1,5 @@
+---
+"@nickarocho-js-changeset-poc/core": minor
+---
+
+Removes unused warning and error logger functions
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,12 +2,4 @@ export const log = (message: any, ...rest: any[]) => {
   console.log(message, ...rest);
 };
 
-export const warn = (message: any, ...rest: any[]) => {
-  console.warn(message, ...rest);
-};
-
-export const error = (message: any, ...rest: any[]) => {
-  console.error(message, ...rest);
-};
-
 export const packageName = "core";
